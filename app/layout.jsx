d09c1c92a26fe325c7c76ebf747cec22263551c6
cjfarrell86 <cjfarrell86@gmail.com>
2026-06-29 15:17:51 -0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "ClientBase Demo",
+  description: "CRM Demo",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
